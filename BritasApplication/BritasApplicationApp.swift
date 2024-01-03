8889c1af5ce5da12c0d3aa5500b4d5362c3653a3
pontusedugrade <pontus.berggren@edu.edugrade.se>
2024-01-03 12:10:35 +0100

    Initial Commit

---
+++ BritasApplication/BritasApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BritasApplicationApp.swift
+//  BritasApplication
+//
+//  Created by Pontus Berggren on 2024-01-03.
+//
+
+import SwiftUI
+
+@main
+struct BritasApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BritasApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BritasApplication
+//
+//  Created by Pontus Berggren on 2024-01-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
